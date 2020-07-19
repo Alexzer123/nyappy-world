d6d8de4f0b59000a04cd64671474246bd9629f75
Alex Horchev <aleksandr1995g@yandex.ru>
2020-07-19 14:27:51 +0300

    Header creation and styling.

---
+++ shop/include/header.php
@@ -0,0 +1,31 @@
+<div class="container-fluid">
+    <div class="info-content">
+        <ul class="address">
+            <li> <i class="fa fa-map-marker" aria-hidden="true"></i> </li>
+            <li> Sunt eu consectetur. </li>
+        </ul> <!-- /.address -->
+
+        <ul class="hours">
+            <li> <i class="fa fa-clock-o" aria-hidden="true"></i> </li>
+            <li> Excepteur mollit in. </li>
+        </ul> <!-- /.address -->
+
+        <ul class="phone">
+            <li> <i class="fa fa-phone" aria-hidden="true"></i> </li>
+            <li> Ea commodo reprehenderit. </li>
+        </ul> <!-- /.address -->
+    </div> <!-- /.info-content -->
+
+    <div class="logo"><img src="img/1.jpg" alt=""></div> <!-- /.logo -->
+
+    <div class="menu">
+        <nav>
+            <ul>
+                <li> <a href="#"> Home </a> </li>
+                <li> <a href="#"> Products </a> </li>
+                <li> <a href="#"> About </a> </li>
+                <li> <a href="#"> Contacts </a> </li>
+            </ul>
+        </nav>
+    </div> <!-- /.menu -->
+</div> <!-- /.container-fluid -->
---
+++ shop/index.php
@@ -0,0 +1,7 @@
+<?php include('function/header.php'); ?>
+
+<header> <?php include('include/header.php'); ?> </header>
+
+<footer></footer>
+
+<?php include('function/footer.php');
