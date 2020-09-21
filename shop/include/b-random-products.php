f2ba7cc5ae2b0f53282086e044e52e52bba6ee66
Alex Horchev <aleksandr1995g@yandex.ru>
2020-09-21 14:00:23 +0300

    End of random products block creation.

--- shop/include/b-random-products.php
+++ shop/include/b-random-products.php
@@ -1,25 +1,24 @@
 <div class="b-random-products">
     <div class="container-fluid">
 
-        <div class="row row-cols-4">
-            <div class="col">
-                <img class="img-fluid" src="img/products/1.jpg" alt="">
-            </div> <!-- /.col -->
-            <div class="col">
-                <img class="img-fluid" src="img/products/2.jpg" alt="">
-            </div> <!-- /.col -->
-            <div class="col">
-                <img class="img-fluid" src="img/products/3.jpg" alt="">
-            </div> <!-- /.col -->
-            <div class="col">
-                <img class="img-fluid" src="img/products/1.jpg" alt="">
-            </div> <!-- /.col -->
-            <div class="col">
-                <img class="img-fluid" src="img/products/2.jpg" alt="">
-            </div> <!-- /.col -->
-            <div class="col">
-                <img class="img-fluid" src="img/products/3.jpg" alt="">
-            </div> <!-- /.col -->
+        <div class="row row-cols-5">
+
+            <div class="product-card">
+                <a class="produсt-card-link" href="">
+                    <div class="produсt-card-layer"></div> <!-- /.produсt-card-layer -->
+
+                    <div class="product-card-content">
+                        <img class="product-card-img-top" src="img/products/1.jpg" alt="">
+
+                        <div class="product-card-body">
+                            <span class="product-card-title"> Title </span> <!-- /.product-card-title -->
+
+                            <span class="product-card-price"> Price </span> <!-- /.product-card-price -->
+                        </div> <!-- /.product-card-body -->
+                    </div> <!-- /.product-card-content -->
+                </a> <!-- /.produсt-card-link -->
+            </div> <!-- /.product-card -->
+
         </div> <!-- /.row row-cols-4 -->
 
     </div> <!-- /.container-fluid -->
